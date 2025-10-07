ce57a537695f7067404db211bba7e4c17e75f086
Shivam <official.shivam0103@gmail.com>
2025-10-07 15:25:45 +0530

    complete Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { dummyCreationData } from "../assets/assets";
 import { CreativeCommons, Gem, Sparkles } from "lucide-react";
 import { Protect } from "@clerk/clerk-react";
+import CreationItem from "../components/CreationItem";
 
 const Dashboard = () => {
   const [creation, setCreations] = useState([]);
@@ -21,7 +22,7 @@ const Dashboard = () => {
         <div className="flex justify-between items-center w-72 p-4 px-6 bg-white rounded-xl border border-gray-200">
           <div className="text-slate-600">
             <p className="text-sm">Total Creations</p>
-            <h2 className="text-xl font-semibold">{CreativeCommons.length}</h2>
+            <h2 className="text-xl font-semibold">{creation.length}</h2>
           </div>
           <div className="w-10 h-10 rounded-lg bg-gradient-to-br from-[#3588F2] to-[#0BB0D7] text-white flex items-center justify-center">
             <Sparkles className="w-5 text-white" />
@@ -44,6 +45,9 @@ const Dashboard = () => {
 
       <div className="space-y-3">
         <p className="mt-6 mb-4">Recent Creations</p>
+        {
+          creation.map((item) => <CreationItem key={item.id} item={item} />)
+        }
       </div>
 
       
